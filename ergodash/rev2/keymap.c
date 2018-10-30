cd7098f119ec80f320f81aecba0107b2eef3b7cb
mkaigawa <15917671+mkaigawa@users.noreply.github.com>
2018-10-30 13:20:15 +0900

    Update keymap.c

--- ergodash/rev2/keymap.c
+++ ergodash/rev2/keymap.c
@@ -27,21 +27,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    * |  `   |   1  |   2  |   3  |   4  |   5  |  ESC |                    |  DEL |   6  |   7  |   8  |   9  |   0  |  -   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | LGui |   Q  |   W  |   E  |   R  |   T  | EISU |                    |   =  |   Y  |   U  |   I  |   O  |   P  |  \   |
+   * | LGui |   Q  |   W  |   E  |   R  |   T  |  ESC |                    |   =  |   Y  |   U  |   I  |   O  |   P  |  \   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  |   [  |                    |   ]  |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |      |                    |      |   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+      +------+------+------+      +------+------+------+------+-------------|
-   * | Alt  | Raise|      |      |||||||| Space| Tab  |      ||||||||      | Enter| Bksp |||||||| Lower|      |      |      |
+   * | Alt  |  Up  | Down | EISU |||||||| Space| Tab  |      ||||||||      | Enter| Bksp |||||||| Lower| Left | Right|  Alt |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_ESC,                         KC_DEL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
-    KC_LGUI, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    EISU,                           KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
+    KC_LGUI, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_ESC,                         KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC ,                       KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_TAB ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LALT, KC_NO,   KC_NO,   KC_NO,            KC_SPC,  KC_TAB ,_______,        _______,KC_ENT , KC_BSPC,          LOWER,   KC_NO,   KC_NO,   KC_NO    \
+    KC_LALT, KC_UP,   KC_DOWN, EISU,             KC_SPC,  KC_TAB ,_______,        _______,KC_ENT , KC_BSPC,          LOWER,   KC_LEFT, KC_RGHT, KC_RALT    \
   ),
 
   /* Lower
