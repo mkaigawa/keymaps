33e37b1c0880e72141c5e8ba3fbedf6f682a652e
mkaigawa <mkaigawa@valuecommerce.co.jp>
2018-12-14 14:35:52 +0900

    update keymap

--- ergodash/rev2/keymap.c
+++ ergodash/rev2/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LGUI, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    EISU ,                          KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC ,                       KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    KC_LALT, KC_UP,   KC_DOWN, RAISE,             KC_TAB,  KC_SPC ,_______,        _______,KC_ENT , KC_BSPC,          LOWER,   KC_LEFT, KC_RGHT, KC_RALT  \
+    KC_LALT, KC_UP,   KC_DOWN, RAISE,            KC_TAB,  KC_SPC ,_______,        _______,KC_ENT , KC_BSPC,          LOWER,   KC_LEFT, KC_RGHT, KC_RALT  \
   ),
 
   /* Lower
@@ -50,9 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * |      |      |MsBtn4| MsUp |MsBtn5|      |      |                    |      |      | Home |  End |      |      |      |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |      |      |MsLeft|MsDown|MsRiht|      |      |                    |      | Left | Down |  Up  | Right|      |      |
+   * |      | EISU |MsLeft|MsDown|MsRiht|      |      |                    |      | Left | Down |  Up  | Right|      |      |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |                    |      |      |WhDown| WhUp |      |      |      |
+   * |      |      |  Del |      |      |      |      |                    |      |      |WhDown| WhUp |      |      |      |
    * |-------------+------+------+------+------+      +------+------+------+      +------+------+------+------+-------------|
    * |      |      |      |      |||||||| Lclk | Rclk |      ||||||||      |      |      ||||||||      |      |      |      |
    * ,----------------------------------------------------------------------------------------------------------------------.
@@ -60,8 +60,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT(
     KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
     KC_TRNS, KC_TRNS, KC_BTN4, KC_MS_U, KC_BTN5, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_HOME, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_WH_D, KC_WH_U, KC_TRNS, KC_TRNS, KC_TRNS, \
+    KC_TRNS, EISU,    KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS, \
+    KC_TRNS, KC_TRNS, KC_DEL,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                        KC_TRNS, KC_TRNS, KC_WH_D, KC_WH_U, KC_TRNS, KC_TRNS, KC_TRNS, \
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_BTN1, KC_BTN2,_______,        _______,KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  \
   ),
 
